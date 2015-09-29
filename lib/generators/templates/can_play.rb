7043a71490df59751b8bf160a88281e08aea19d3
happyming <339755551@qq.com>
2015-09-29 10:37:35 +0800

    超级角色可通过传入代码块确定

--- lib/generators/templates/can_play.rb
+++ lib/generators/templates/can_play.rb
@@ -14,6 +14,9 @@ CanPlay::Config.setup do |config|
   # super_roles表示无需分配权限既可拥有所有权限的角色。
   config.super_roles = ['超级管理员']
 
+  # 也可以传入代码块，若使代码块，则会传入形参user，指代当前用户，代码块返回的结果为true，则当前登录用户的当前角色则有无限权限。。
+  #config.super_roles = ->(role) { 1 == user.current_role.try(:id) }
+
   # 判断角色是否符合的方法。
   config.role_judge_method = 'role_is?'
 end
